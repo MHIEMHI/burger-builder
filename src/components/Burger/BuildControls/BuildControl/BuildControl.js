8bca31ef08c667720b9a1a1cc15245bb9f0bb5de
MHI <mehdihajoujiidrissi@gmail.com>
2020-05-02 13:47:05 +0000

    134. Removing Ingredients Safely

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,19 @@ import classes from './BuildControl.module.css';
 const buildControl = props => (
 	<div className={classes.BuildControl}>
 		<div className={classes.Label}>{props.label}</div>
-		<button onClick={props.added} className={classes.Less}>Less</button>
-		<button onClick={props.added} className={classes.More}>More</button>
+		<button
+			disabled={props.disabled}
+			onClick={props.removed}
+			className={classes.Less}
+		>
+			Less
+		</button>
+		<button
+			onClick={props.added}
+			className={classes.More}
+		>
+			More
+		</button>
 	</div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -39,16 +39,39 @@ class BurgerBuilder extends Component
 
 	removeIngredientHandler = type =>
 	{
-
+		const oldCount = this.state.ingredients[type];
+		if (oldCount <= 0)
+		{
+			return;
+		}
+		const updatedCount = oldCount - 1;
+		const updatedIngredient = {
+			...this.state.ingredients
+		};
+		updatedIngredient[type] = updatedCount;
+		const priceDeduction = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice - priceDeduction;
+		this.setState({ ingredients: updatedIngredient, totalPrice: newPrice });
 	};
 
 	render()
 	{
+		const disableInfo = {
+			...this.state.ingredients
+		};
+		for (let key in disableInfo)
+		{
+			disableInfo[key] = disableInfo[key] <= 0;
+		}
+
 		return (
 			<Aux>
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls
 					ingredientAdded={this.addIngredientHandler}
+					ingredientRemoved={this.removeIngredientHandler}
+					disabled={disableInfo}
 				/>
 			</Aux>
 		);
