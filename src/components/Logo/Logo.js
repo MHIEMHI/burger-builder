7083157c14b62d6fc555d955263f1b51becb853a
MHI <mehdihajoujiidrissi@gmail.com>
2020-05-03 15:41:59 +0000

    147. Working on Responsive Adjustments

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,10 @@ import burgerLogo from '../../../src/assets/images/burger-logo.png';
 import classes from './Logo.module.css';
 
 export const Logo = props => (
-	<div className={classes.Logo}>
+	<div
+		className={classes.Logo}
+		style={{ height: props.height }}
+	>
 		<img src={burgerLogo} alt="MyBurger" />
 	</div>
 );
