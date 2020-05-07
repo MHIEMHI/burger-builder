3cbcc43527040fd3c352e2c7d90ee8351fd2e7a9
MHI <mehdihajoujiidrissi@gmail.com>
2020-05-07 22:16:55 +0000

    192. Preparing the Project For Routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,51 +1,12 @@
 import React, { Component } from 'react';
 
-import Post from '../../components/Post/Post';
 import './Blog.css';
-import axios from 'axios';
+import Posts from '../Posts/Posts';
 
 class Blog extends Component
 {
-	state = {
-		posts: [],
-		selectedPostId: null,
-		error: false
-	};
-
-	postSelectedHandler = id =>
-	{
-		this.setState({ selectedPostId: id });
-	};
-
-	componentDidMount()
-	{
-		axios.get("/posts")
-			.then(response =>
-			{
-				const posts = response.data.slice(0, 4);
-				const updatedPosts = posts.map(post => ({ ...post, author: 'Max' }));
-				this.setState({ posts: updatedPosts });
-			})
-			.catch(error =>
-			{
-				this.setState({ error: true });
-			});
-	}
-
 	render()
 	{
-		let posts = <p style={{ textAlign: 'center' }}>Somthing went wrong !</p>;
-		if (!this.state.error)
-		{
-			posts = this.state.posts.map(post => (
-				<Post
-					key={post.id}
-					title={post.title}
-					author={post.author}
-					clicked={() => this.postSelectedHandler(post.id)}
-				/>
-			));
-		}
 		return (
 			<div className="Blog">
 				<header>
@@ -56,9 +17,7 @@ class Blog extends Component
 						</ul>
 					</nav>
 				</header>
-				<section className="Posts">
-					{posts}
-				</section>
+				<Posts />
 			</div>
 		);
 	}
