3ac103c644ea9b45b01e8598311a1154ad6f7007
MHI <mehdihajoujiidrissi@gmail.com>
2020-05-08 17:12:08 +0000

    217. Routing and Server Deployment

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component
   render()
   {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <div className="App">
           <Blog />
           <Switch>
