201deec039e22cd68bfb35f895a91ae1d9df3916
MHI <mehdihajoujiidrissi@gmail.com>
2020-05-09 00:13:15 +0000

    227. Order Submission & Passing Data Between Pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -48,38 +48,12 @@ class BurgerBuilder extends Component
 
 	purchaseContinueHandler = () => 
 	{
-		//alert('You Continue!');
-		// this.setState({ loading: true });
-		// const order = {
-		// 	ingredients: this.state.ingredients,
-		// 	price: this.state.totalPrice,
-		// 	customer:
-		// 	{
-		// 		name: 'Mehdi',
-		// 		address:
-		// 		{
-		// 			street: 'Test street',
-		// 			zipCode: '50010',
-		// 			country: 'Morroco'
-		// 		},
-		// 		email: 'test@test.com'
-		// 	},
-		// 	deliveryMethod: 'fastest'
-		// };
-		// axios.post('/orders.json', order)
-		// 	.then(response =>
-		// 	{
-		// 		this.setState({ loading: false, purchasing: false });
-		// 	})
-		// 	.catch(error =>
-		// 	{
-		// 		this.setState({ loading: false, purchasing: false });
-		// 	});
 		const queryParams = [];
 		for (let i in this.state.ingredients)
 		{
 			queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
 		}
+		queryParams.push('totalPrice=' + this.state.totalPrice);
 		const queryString = queryParams.join('&');
 		this.props.history.push({
 			pathname: '/checkout',
