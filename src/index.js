7b013d70d3a1c171213d594d4ec303ab7a2afd83
MHI <mehdihajoujiidrissi@gmail.com>
2020-05-10 18:59:21 +0000

    301. Preparing the Folder Structure

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducer from './store/reducer';
+import reducer from './store/reducers/burgerBuilderReducer';
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
