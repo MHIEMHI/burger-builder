2f47eb8c6196c0bf15dadd6a30a4d0f39b2eeadd
MHI <mehdihajoujiidrissi@gmail.com>
2020-05-10 14:42:04 +0000

    289. Handling Asynchronous Code

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -33,11 +33,21 @@ export const subtract = value =>
 };
 
 export const STORE_RESULT = 'STORE_RESULT';
-export const storeResult = result =>
+export const saveResult = result =>
 {
 	return {
 		type: STORE_RESULT,
 		result
+	};
+};
+export const storeResult = result =>
+{
+	return dispatch =>
+	{
+		setTimeout(() =>
+		{
+			dispatch(saveResult(result));
+		}, 2000);
 	};
 };
 
