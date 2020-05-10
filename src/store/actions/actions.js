85656bfae7e0a74dee49f6a8e427e48c059a2a1d
MHI <mehdihajoujiidrissi@gmail.com>
2020-05-10 14:32:45 +0000

    288. Action Creators & Async Code

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,13 +1,51 @@
 export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const ADD = 'ADD';
-export const SUBTRACT = 'SUBTRACT';
-export const STORE_RESULT = 'STORE_RESULT';
-export const DELETE_RESULT = 'DELETE_RESULT';
-
 export const increment = () =>
 {
 	return {
 		type: INCREMENT
+	};
+};
+
+export const DECREMENT = 'DECREMENT';
+export const decrement = () =>
+{
+	return {
+		type: DECREMENT
+	};
+};
+
+export const ADD = 'ADD';
+export const add = value =>
+{
+	return {
+		type: ADD,
+		value
+	};
+};
+
+export const SUBTRACT = 'SUBTRACT';
+export const subtract = value =>
+{
+	return {
+		type: SUBTRACT,
+		value
+	};
+};
+
+export const STORE_RESULT = 'STORE_RESULT';
+export const storeResult = result =>
+{
+	return {
+		type: STORE_RESULT,
+		result
+	};
+};
+
+export const DELETE_RESULT = 'DELETE_RESULT';
+export const deleteResult = result =>
+{
+	return {
+		type: DELETE_RESULT,
+		result
 	};
 };
