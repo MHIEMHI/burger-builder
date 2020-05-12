0b877c148b6c24e127194dd00760258970441736
MHI <mehdihajoujiidrissi@gmail.com>
2020-05-12 17:03:52 +0000

    311. Redirect to Improve UX

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -19,19 +19,24 @@ class Checkout extends Component
 
 	render()
 	{
-		return (
-			<div>
-				<CheckoutSummary
-					ingredients={this.props.ingredients}
-					checkoutCancelled={this.onCheckoutCancelled}
-					checkoutContinued={this.onCheckoutContinued}
-				/>
-				<Route
-					path={this.props.match.path + '/contact-data'}
-					component={ContactData}
-				/>
-			</div>
-		);
+		let summary = <Redirect to="/" />;
+		if (this.props.ingredients)
+		{
+			summary = (
+				<div>
+					<CheckoutSummary
+						ingredients={this.props.ingredients}
+						checkoutCancelled={this.onCheckoutCancelled}
+						checkoutContinued={this.onCheckoutContinued}
+					/>
+					<Route
+						path={this.props.match.path + '/contact-data'}
+						component={ContactData}
+					/>
+				</div>
+			);
+		}
+		return summary;
 	}
 }
 
