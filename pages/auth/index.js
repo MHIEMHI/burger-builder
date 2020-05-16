a72ed049c1641a6afea6b02f44b08fce607c79e2
MHI <mehdihajoujiidrissi@gmail.com>
2020-05-16 16:05:39 +0000

    387. Setting Up a Project

---
+++ pages/auth/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AuthIndexPage = () =>
+{
+	return (
+		<div>
+			<h1>The Auth index page</h1>
+		</div>
+	);
+};
+
+export default AuthIndexPage;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndexPage = () =>
+{
+	return (
+		<div>
+			<h1>The Main page</h1>
+		</div>
+	);
+};
+
+export default IndexPage;
