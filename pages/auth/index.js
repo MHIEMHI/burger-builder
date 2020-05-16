ef5e1da3761cd8684b57586db4634df7a86dc158
MHI <mehdihajoujiidrissi@gmail.com>
2020-05-16 16:14:56 +0000

    389. Next.js & Components & Pages

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import User from '../../components/User';
 
 const AuthIndexPage = () =>
 {
 	return (
 		<div>
 			<h1>The Auth index page</h1>
+			<User name='MHI' age='28' />
 		</div>
 	);
 };
