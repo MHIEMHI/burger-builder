4fd0b7df54c48c4d5dcf7f15ac8c7cc076b63fb6
MHI <mehdihajoujiidrissi@gmail.com>
2020-05-17 16:54:52 +0000

    441. Loading Errors & State Batching

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,10 +3,13 @@ import React, { useState, useCallback } from 'react';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 
 function Ingredients()
 {
 	const [ingredients, setIngredients] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
 
 	const filteredIngredientsHandler = useCallback(filteredIngredients =>
 	{
@@ -15,6 +18,7 @@ function Ingredients()
 
 	const addIngredientHandler = ingredient =>
 	{
+		setIsLoading(true);
 		fetch(`${process.env.REACT_APP_BASE_URL}/ingredients.json`, {
 			method: 'POST',
 			body: JSON.stringify(ingredient),
@@ -22,25 +26,38 @@ function Ingredients()
 		}).then(response => response.json())
 			.then(responseData =>
 			{
+				setIsLoading(false);
 				setIngredients(prevState => [...prevState, { id: responseData.name, ...ingredient }]);
-
 			});
 	};
 
 	const removeIngredientHandler = id =>
 	{
+		setIsLoading(true);
 		fetch(`${process.env.REACT_APP_BASE_URL}/ingredients/${id}.json`, {
 			method: 'DELETE',
 		}).then(response => response.json())
 			.then(responseData =>
 			{
+				setIsLoading(false);
 				setIngredients(prevState => prevState.filter(ing => ing.id !== id));
+			})
+			.catch(error =>
+			{
+				setIsLoading(false);
+				setError(error.message);
 			});
+	};
+
+	const clearError = () =>
+	{
+		setError(null);
 	};
 
 	return (
 		<div className="App">
-			<IngredientForm addIngredient={addIngredientHandler} />
+			{error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+			<IngredientForm addIngredient={addIngredientHandler} loading={isLoading} />
 
 			<section>
 				<Search setIngredients={filteredIngredientsHandler} />
